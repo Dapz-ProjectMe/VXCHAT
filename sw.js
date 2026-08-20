690e39e39dfb43ad6c4174a1a51ecefad179f518
Dapz-ProjectMe <budi060722@gmail.com>
2026-08-20 18:48:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vxchat-v2";
+const CACHE = "vxchat-v3";
 const ASSETS = ["./", "./index.html", "./login.html", "./register.html", "./chat.html", "./css/style.css"];
 
 self.addEventListener("install", (event) => {
